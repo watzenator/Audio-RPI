6d92e9090b5c08ece6dd88d816563513f5122e86
Suirevax <richard.janssen@student.hu.nl>
2019-04-01 12:56:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 int main(){
-  std::cout << "start playing audio";
+  std::cout << "start playing audio\n";
   system("omxplayer /home/pi/Audio-RPI/dog-bark1.wav");
-  std::cout << "done playing";
+  std::cout << "done playing\n";
 }
